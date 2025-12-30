3e440d275b719689719d6f7c725e1026cf890ff1
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2025-12-30 14:47:12 +0530

    Add files via upload

---
+++ Classwork/program412.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+
+struct node
+{
+    int no;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+
+void InsertFirst(PPNODE first,PPNODE last,int no)
+{}
+void InsertLast(PPNODE first,PPNODE last,int no)
+{}
+void DeleteFirst(PPNODE first,PPNODE last)
+{
+}
+void DeleteLast(PPNODE first,PPNODE last)
+{
+}
+
+void Display(PNODE first)
+{}
+
+int Count(PNODE first,PNODE last)
+{
+    return 0;
+}
+
+void InsertAtPos(PPNODE first,PPNODE last,int no,int pos)
+{}
+void DeleteAtPos(PPNODE first,PPNODE last,int pos)
+{
+}
+
+int main()
+{
+    PNODE head=NULL;
+    PNODE tail=NULL;
+
+    return 0;
+}
