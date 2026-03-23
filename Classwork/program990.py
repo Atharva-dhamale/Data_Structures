208fdb47b76754145b0ddb506052f9c152499aac
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2026-03-23 20:43:19 +0530

    Add files via upload

---
+++ Classwork/program990.py
@@ -0,0 +1,96 @@
+
+#Done
+class Node:
+
+    def __init__(self,value):
+        self.data=value
+        self.next=None
+
+class SinglyLL:
+
+    #Done
+    def __init__(self):
+        self.first=None
+        self.iCount=0
+
+    #Done
+    def InsertFirst(self,no):
+        newn=Node(no)
+
+        if(self.first==None):   #LL is empty
+            self.first=newn
+
+        else:                   #It contains atleast one node
+            newn.next=self.first
+            self.first=newn
+
+        self.iCount=self.iCount+1
+        
+    #Done
+    def InsertLast(self,no):
+        newn=Node(no)
+
+        if(self.first==None):   #LL is empty
+            self.first=newn
+
+        else:
+            temp=self.first
+
+            while(temp.next!=None):
+                temp=temp.next
+            
+            temp.next=newn
+
+        self.iCount=self.iCount+1
+    
+    def InsertAtPos(self,no,pos):
+        pass
+    
+    def DeleteFirst(self):
+        pass
+    
+    def DeleteLast(self):
+        pass
+    
+    def DeleteAtPos(self,pos):
+        pass
+    
+    #Done
+    def Count(self):
+        return self.iCount
+    
+    #Done
+    def Display(self):
+        
+        temp=self.first
+
+        while(temp!=None):
+            print("| ",temp.data," |->",end=" ")
+            temp=temp.next
+
+        print("None")
+
+    
+def main():
+
+    sobj=SinglyLL()
+
+    sobj.InsertFirst(101)
+    sobj.InsertFirst(51)
+    sobj.InsertFirst(21)
+    sobj.InsertFirst(11)
+
+    print("Elements of Linked List are : ")
+    sobj.Display()
+    print("Number of elemnts in Linked List are : ",sobj.Count())
+
+    sobj.InsertLast(111)
+    sobj.InsertLast(121)
+
+    print("Elements of Linked List are : ")
+    sobj.Display()
+    print("Number of elemnts in Linked List are : ",sobj.Count())
+
+
+if __name__=="__main__":
+    main()
